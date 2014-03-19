4b1e2aa417b4b85525d804534ab787f9e19ba1dd
Sifungurux <jonas_pedersen@hotmail.com>
2014-03-19 10:59:05 +0100

    moved scripts

---
+++ Nagios Passive/Check_Server.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+HOST={your host}
+SERVICE_DISK=BACKUP
+SERVICE_LOAD=LOAD
+NSCAHOST={Your IP}
+NOW=$(date +"%m-%d-%y %r")
+CURRENTDIR=`pwd`
+
+
+${CURRENTDIR}/check_disk -w 300 -c 100 -p / >> nagios_disk_check.log
+VAR="$(${CURRENTDIR}/check_disk -w 300 -c 200 -p / )"
+RETVAL=$?
+
+if [ ${RETVAL} -eq 2 ] ; then
+  echo "$HOST\t$SERVICE_DISK\t$RETVAL\t${NOW} - ${VAR}" | /usr/sbin/send_nsca -H $NSCAHOST -c /etc/send_nsca.cfg
+fi
+if [ ${RETVAL} -eq 1 ] ; then
+  echo "$HOST\t$SERVICE_DISK\t$RETVAL\t${NOW} - ${VAR}" | /usr/sbin/send_nsca -H $NSCAHOST -c /etc/send_nsca.cfg
+fi
+if [ ${RETVAL} -eq 0 ] ; then
+  echo "$HOST\t$SERVICE_DISK\t$RETVAL\t${NOW} - ${VAR}" | /usr/sbin/send_nsca -H $NSCAHOST -c /etc/send_nsca.cfg 
+fi
+
+
+
+${CURRENTDIR}/check_load -w 7,12,15 -c 15,20,25 >> nagios_load_check.log
+VAR="$(${CURRENTDIR}/check_load -w 7,12,15 -c 15,20,25)"
+RETVAL=$?
+
+if [ ${RETVAL} -eq 2 ] ; then
+  echo "$HOST\t$SERVICE_LOAD\t$RETVAL\t${NOW} - ${VAR}" | /usr/sbin/send_nsca -H $NSCAHOST -c /etc/send_nsca.cfg
+fi
+if [ ${RETVAL} -eq 1 ] ; then
+  echo "$HOST\t$SERVICE_LOAD\t$RETVAL\t${NOW} - ${VAR}" | /usr/sbin/send_nsca -H $NSCAHOST -c /etc/send_nsca.cfg
+fi
+if [ ${RETVAL} -eq 0 ] ; then
+  echo "$HOST\t$SERVICE_LOAD\t$RETVAL\t${NOW} - ${VAR}" | /usr/sbin/send_nsca -H $NSCAHOST -c /etc/send_nsca.cfg
+fi
+
+
+# Add more checks natively from nagios plugin folder 
+exit 0
